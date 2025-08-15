9b0f336ba733c22dd8283e8230c6b858b317ab60
Matthew Smith <msmith@qsrautomations.com>
2025-08-15 15:47:01 -0400

    updates workshop test

--- tests/workshop-day-1.spec.ts
+++ tests/workshop-day-1.spec.ts
@@ -1,19 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 
-test("should load homepage and check title", async ({ page }) => {
+test("Our first test!", async ({ page }) => {
 
-
-});
-
-test("should open modal on button click", async ({ page }) => {
-
-});
-
-test("should submit login form", async ({ page }) => {
-
-});
-
-test("should show navbar on homepage", async ({ page }) => {
 
 });
